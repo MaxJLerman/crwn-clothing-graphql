4d322b6e40eaf2c4c64f5833b351206fd86d8591
Max <93211492+VivaLaBagget@users.noreply.github.com>
2022-07-19 21:26:42 +0100

    added shopping cart icon

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,7 @@ import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
 import './navigation.styles.scss';
 
 const Navigation = () =>
@@ -33,7 +34,7 @@ const Navigation = () =>
                             <Link className="navigation-link" to='/authentication'>sign in</Link>
                         )
                     }
-                    
+                    <CartIcon />
                 </div>
             </div>
             <Outlet />
