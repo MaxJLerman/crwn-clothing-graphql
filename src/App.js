85bf2984c50d8d22073a99e14603234d38898c69
MaxJLerman <93211492+MaxJLerman@users.noreply.github.com>
2022-12-01 12:12:41 +0000

    section 14 ep149

--- src/App.js
+++ src/App.js
@@ -6,18 +6,17 @@ import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
-const App = () =>
-{
-    return(
-        <Routes>
-            <Route path='/' element={<Navigation />} >
-                <Route index element={<Home />} /> {/* including "index" makes it so the Home component shows when the path is just '/' */}
-                <Route path='shop/*' element={<Shop />} /> {/* the path suffix of /* means anything after the shop component is rendered in the shop and then the further routing is done inside the shop component */}
-                <Route path='authentication' element={<Authentication />} />
-                <Route path="checkout" element={<Checkout />} />
-            </Route>
-        </Routes>
-    );
-}
+const App = () => {
+  return(
+    <Routes>
+      <Route path='/' element={<Navigation />} >
+        <Route index element={<Home />} /> {/* including "index" makes it so the Home component shows when the path is just '/' */}
+        <Route path='shop/*' element={<Shop />} /> {/* the path suffix of /* means anything after the shop component is rendered in the shop and then the further routing is done inside the shop component */}
+        <Route path='authentication' element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
