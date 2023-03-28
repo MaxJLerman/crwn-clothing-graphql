588be3343760c21b9506bae0f1b14f8ac3bd78f7
Max Lerman <mjl82601@gmail.com>
2023-03-28 15:33:31 +0100

    156 react-redux selectors

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,9 @@
 import { Fragment, useContext } from 'react';
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import { UserContext } from '../../contexts/user.context';
+import { selectCurrentUser } from '../../store/user/user.selector';
 import { CartContext } from '../../contexts/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
@@ -11,7 +12,7 @@ import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component
 import { NavigationContainer, LogoContainer, NavigationLinks, NavigationLink } from './navigation.styles';
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
   
   return (
---
+++ src/store/user/user.selector.js
@@ -0,0 +1,4 @@
+export const selectCurrentUser = (state) => state.user.currentUser;
+// the selector function above extracts off the desired values from the entire redux store
+// whenever state object changes, i.e. log in/out updates the reducer value currentUser, new state object is returned with new value
+// when this happens, useSelector re-runs, currentUser updates and React will re-render the component below
