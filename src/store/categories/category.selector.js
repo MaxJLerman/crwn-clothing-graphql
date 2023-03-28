269c1c14118149eeae026a106776c23daa37263a
Max Lerman <mjl82601@gmail.com>
2023-03-28 17:00:06 +0100

    160 updated category selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,7 @@
-export const selectCategoriesMap = (state) => state.categories.categories.reduce((accumulator, category) => {
-  const { title, items } = category;
-
-  accumulator[title.toLowerCase()] = items;
-  return accumulator;
-}, {});
+export const selectCategoriesMap = (state) => {
+  return state.categories.categories.reduce((accumulator, category) => {
+    const { title, items } = category;
+    accumulator[title.toLowerCase()] = items;
+    return accumulator;
+  }, {});
+};
