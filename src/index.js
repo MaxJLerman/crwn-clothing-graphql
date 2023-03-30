2cfff9de59ac8503ff846230dfa809b8c5e4e58c
Max Lerman <mjl82601@gmail.com>
2023-03-30 20:39:13 +0100

    169 redux-persist

--- src/index.js
+++ src/index.js
@@ -2,23 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-<React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-</React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      {/* loading us used to render [something] until persistor is finished */}
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
 
 reportWebVitals();
