f2909392d899d9a11ac6ad43fdfb543f35780a62
Max Lerman <mjl82601@gmail.com>
2023-04-05 09:36:54 +0100

    192 stripe

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store, persistor } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 import './index.scss';
 
@@ -15,10 +17,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* loading us used to render [something] until persistor is finished */}
+      {/* loading is used to render [something] until persistor is finished */}
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
