15837dc7cdea68ef0bd9d40c69972e7eccc4e48d
Max Lerman <mjl82601@gmail.com>
2023-04-05 10:36:18 +0100

    195

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -13,14 +13,45 @@ const PaymentForm = () => {
 
     if (!stripe || !elements) return; // if no stripe or elements, go back
 
-    
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ amount: 10000 })
+    }).then(res => res.json());
+
+    const clientSecret = response.paymentIntent.client_secret;
+
+    console.log(clientSecret);
+
+    const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Max Lerman'
+        }
+      }
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    }
+
+    else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Payment successful!');
+      }
+    }
   };
   
   return (
     <PaymentFormContainer>
-      <h2>Credit Card Payment:</h2>
-      <CardElement />
-      <Button buttonType={t.INVERTED_BUTTON}>Pay now</Button>
+      <FormContainer onSubmit={paymentHandler}>
+        <h2>Credit Card Payment:</h2>
+        <CardElement />
+        <Button buttonType={t.INVERTED_BUTTON}>Pay now</Button>
+      </FormContainer>
     </PaymentFormContainer>
   );
 };
