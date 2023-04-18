eb97921ed91c9f6adfe9208b81fce79f899fe6d7
Max Lerman <mjl82601@gmail.com>
2023-04-18 14:24:08 +0100

    updated authentication component file

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,45 +1,13 @@
-// the code commented out in this file was used to highlight some of firebase's methods & authentication
-
-//import { useEffect } from 'react';
-//import { getRedirectResult } from 'firebase/auth';
-
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
-// import { authentication, signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils';
 
 import { AuthenticationContainer } from './authentication.styles';
 
 const Authentication = () => {
-  // unused useEffect
-  // useEffect( () =>
-  // {
-  //     async function fetchData()
-  //     {
-  //         const response = await getRedirectResult(authentication);
-          
-  //         if (response)
-  //         {
-  //             const userDocumentReference = await createUserDocumentFromAuthentication(response.user);
-  //         }
-  //     }
-
-  //     fetchData(); // needs to be called
-  // }, []);
-    
-    
-    
   return (
     <AuthenticationContainer>
       <SignInForm />
       <SignUpForm />
-
-      {
-        /* #region unused google redirect */
-        // <button onClick={signInWithGoogleRedirect}>
-        //     sign in with google redirect
-        // </button>
-        /* #endregion */
-      }
     </AuthenticationContainer>
   );
 };
