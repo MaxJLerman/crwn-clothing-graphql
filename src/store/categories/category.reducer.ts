8606903709fe94401d0e065edf2076c0b66d184d
Max Lerman <mjl82601@gmail.com>
2023-04-25 10:15:13 +0100

    converted categoriesReducer action matcher from switch-case statements to to be fully type-safe

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,5 +1,7 @@
+import { AnyAction } from "redux";
+
 import { CATEGORIES_ACTION_TYPES, Category } from "./category.types";
-import { CategoryAction } from "./category.action";
+import { CategoryAction, fetchCategoriesStart, fetchCategoriesSuccess, fetchCategoriesFailed } from "./category.action";
 
 // readonly is necessary because the values below must never be modified
 // this is because with reducers, you never modify the original state => must always spread over and create a new state
@@ -18,34 +20,30 @@ const CATEGORIES_INITIAL_STATE: CategoriesState = {
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {} as CategoryAction // this is called a discriminating union - should only accept actions of the 3 types specified
-) => {
-  // can no longer destructure the type and payload from the action because not all actions have a payload
-  // const { type, payload } = action;
-  // must use action.type and action.payload in thier respective places due to using TypeScript (see below)
-
-  switch (action.type) {
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return {
-        ...state,
-        isLoading: true
-      };
-
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return {
-        ...state,
-        categories: action.payload,
-        isLoading: false
-      };
-
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return {
-        ...state,
-        error: action.payload,
-        isLoading: false
-      };
-
-    default:
-      return state;
+  action = {} as AnyAction
+): CategoriesState => {
+  if (fetchCategoriesStart.match(action)) {
+    return {
+      ...state,
+      isLoading: true
+    };
   };
+
+  if (fetchCategoriesSuccess.match(action)) {
+    return {
+      ...state,
+      categories: action.payload,
+      isLoading: false
+    };
+  };
+
+  if (fetchCategoriesFailed.match(action)) {
+    return {
+      ...state,
+      error: action.payload,
+      isLoading: false
+    };
+  };
+
+  return state; // if none match, return the default state
 };
